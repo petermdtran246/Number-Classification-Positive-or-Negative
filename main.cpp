9a29a4756528a954d29d3faff341d757a7b51500
petermdtran246 <petermdtran246@gmail.com>
2024-05-12 11:02:01 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter your nos: ";
+    cin >> n;
+    if(n>=0)
+        cout << "positive ";
+    else
+        cout << "negative ";
+    return 0;
+}
